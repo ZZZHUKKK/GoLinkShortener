99f8d5bcd79f52a4a2f826aa63e75ae158202f19
Ivan Sharzhukov <neskazhu@example.com~>
2025-05-11 10:43:52 +0300

    Integrated GORM

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"demo/linker/configs"
 	"demo/linker/internal/auth"
+	"demo/linker/pkg/db"
 	"fmt"
 	"net/http"
 )
 
 func main() {
 	conf := configs.LoadConfig()
+	_ = db.NewDb(conf)
 	router := http.NewServeMux()
 	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
 		Config: conf,
